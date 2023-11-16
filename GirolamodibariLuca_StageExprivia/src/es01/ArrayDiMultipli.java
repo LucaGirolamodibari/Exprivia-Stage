2df06071541027f3cd3e1f6cc63ff1e50761fbbc
lucag <lucag@MateBook-di-Luca>
2023-11-16 18:27:09 +0100

    .

--- GirolamodibariLuca_StageExprivia/src/es01/ArrayDiMultipli.java
+++ GirolamodibariLuca_StageExprivia/src/es01/ArrayDiMultipli.java
@@ -1,8 +1,27 @@
 package es01;
 
 public class ArrayDiMultipli {
-	public ArrayDiMultipli() {
-		// TODO Auto-generated constructor stub
+	private int[] multipliArray;
+	int num;
+	int lunghezza;
+
+	public ArrayDiMultipli(int num, int lunghezza) {
+		multipliArray = new int[lunghezza];
+		this.num = num;
+		this.lunghezza = lunghezza;
+
+		for (int i = 0; i < lunghezza; i++) {
+			multipliArray[i] = num * (i + 1);
+		}
+	}
+
+	public int[] getMultipli() {
+		return multipliArray;
+	}
+	
+	@Override
+	public String toString() {
+		return "arrayDiMultipli("  + num + "," + lunghezza + ")➞";
 	}
 
 }
--- GirolamodibariLuca_StageExprivia/src/es01/Main.java
+++ GirolamodibariLuca_StageExprivia/src/es01/Main.java
@@ -1,9 +1,21 @@
 package es01;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayDiMultipli arrayDiMultipli = new ArrayDiMultipli();
+		ArrayDiMultipli arrayDiMultipli = new ArrayDiMultipli(7,5);
+		int[] risultato = arrayDiMultipli.getMultipli();
+		System.out.println(arrayDiMultipli.toString() + Arrays.toString(risultato));
+		
+		ArrayDiMultipli arrayDiMultipli2 = new ArrayDiMultipli(12,10);
+		int[] risultato2 = arrayDiMultipli2.getMultipli();
+		System.out.println(arrayDiMultipli2.toString() + Arrays.toString(risultato2));
+		
+		ArrayDiMultipli arrayDiMultipli3 = new ArrayDiMultipli(17,6);
+		int[] risultato3 = arrayDiMultipli3.getMultipli();
+		System.out.println(arrayDiMultipli3.toString() + Arrays.toString(risultato3));
 
 	}
 
